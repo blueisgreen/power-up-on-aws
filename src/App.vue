d03c08d52851f215bb1fb01f610712fdd7754a61
Dave Mount <davemount@gmail.com>
2021-07-04 10:09:37 -0700

    Set up first API w/model. Start to connect UI.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To Do App</h1>
+    <input type="text" v-model="name" placeholder="To Do Name" />
+    <input type="text" v-model="description" placeholder="To Do Description" />
+    <button @click="createTodo">Create To Do</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify'
+import { createTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import { onCreateTodo } from './graphql/subscriptions'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  async created() {
+    this.getTodos()
+    this.subscribe()
+  },
+  data() {
+    return {
+      name: '',
+      description: ''
+    }
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this
+      if (!name || !description) return;
+      const todo = { name, description }
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo},
+      })
+      this.name = ''
+      this.description = ''
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      })
+      this.todos = todos.data.listTodos.items;
+    },
+    subscribe() {
+      API.graphql({ query: onCreateTodo })
+        .subscribe({
+          next: (eventData) => {
+            let todo = eventData.value.data.onCreateTodo
+            if (this.todos.some(item => item.name === todo.name)) return
+            this.todos = [...this.todos, todo]
+          }
+        })
+    }
   }
 }
 </script>
